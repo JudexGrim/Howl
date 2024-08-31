c92700e374d998b20e5cc42c6d86292a09042f61
JudexGrim <om.jumaili01@gmail.com>
2024-09-01 00:04:01 +0300

    Created Initial Solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Howl.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
